fbd43783f7d6db15610ffef495dd8f905f636829
Savanna <savannaos@users.noreply.github.com>
2016-02-28 17:08:06 -0500

    Update Main.java
    This is an updated version that includes the randomization process. It also hopes to generalize this process

--- Main.java
+++ Main.java
@@ -1,25 +1,48 @@
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args)
-    {
-        String[] initials = {"FN","AH", "SK", "AW", "HH", "ME", "ML", "JF", "JS", "SR", "SB", "LG", "NH", "SS", "SV", "CW" };
+/**
+ * Generates a list of n distinct, random numbers. 
+ * @author savanna
+ *
+ */
+public class RandomNumbers {
+	    public static int[] randomNum(int amount) {
+	    	ArrayList<Integer> list = new ArrayList<Integer>();
+	        for (int i=0; i<amount; i++) {
+	            list.add(new Integer(i));
+	        }
+	        int[] randomArray = new int[amount];
+	        Collections.shuffle(list);
+	        for (int i=0; i<amount; i++) {
+	            randomArray[i] = list.get(i);
+	            //I want to make sure the index is not the same as the value!
+	        }
+	        return randomArray;
+	    }
 
-
-       int[] cards = {10, 15, 9, 8, 7, 5, 6, 4, 3, 1, 16, 13, 14, 2, 11, 12};
-        Scanner sc = new Scanner(System.in);
-
-        while(true)
-        {
-            System.out.println("Enter your first and last initial (uppercase)");
-            String inputText = sc.nextLine();
-            for (int i = 0; i < initials.length; i++)
-            {
-                if (initials[i].equals(inputText))
-                {
-                    System.out.println("Card " + cards[i]);
-                }
-            }
-        }
-    }
-}
+	    public static void main(String[] args){
+	    	String[] initials = {"FN","AH", "SK", "AW", "HH", "ME", "ML", "JF", "JS", "SR", "SB", "LG", "NH", "SS", "SV", "CW" };
+	    	int amount = initials.length-1;
+	    	int[] randomAssignments = randomNum(amount);
+	    	for (int i=0; i<randomAssignments.length; i++)
+	        {
+	        	if(i == randomAssignments[i]){
+	        		System.out.println("array index equals element, run again");
+	        	}
+	        }
+	    	Scanner sc = new Scanner(System.in);
+	    	while(true){
+	    		System.out.println("Enter your first and last initial (uppercase)");
+	    		String inputText = sc.nextLine();
+	    		for (int i = 0; i < initials.length; i++){
+	    			if (initials[i].equals(inputText)){
+	    				int index = randomAssignments[i];
+	    				System.out.println("Your person's initials are: " + initials[index]);
+	    				
+	    				}
+	    			}
+	    		}
+	    	}
+	}
